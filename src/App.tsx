3892f3fe0fd2027d3b0d1e02b4853144558cfb54
SELWA Raphael <selwa.raphael@gmail.com>
2024-06-26 22:55:05 +0200

    ✨ Category filter

--- src/App.tsx
+++ src/App.tsx
@@ -8,30 +8,52 @@ import { useCustomContext } from "@/custom-context"
 
 const App = () => {
   const { movies, setMovies } = useCustomContext()
-
-  const [searchParams, setSearchParams] = useSearchParams()
   const moviesCategories = [...new Set(movies.map((movie) => movie.category))]
 
+  const [searchParams, setSearchParams] = useSearchParams()
+  const categoryFilter = searchParams.get("c")
+  const filterMovies = (category: string) => {
+    if (category === "default") {
+      const newSearchParams = searchParams
+      newSearchParams.delete("c")
+      setSearchParams(newSearchParams)
+
+      return
+    }
+    setSearchParams({ ...searchParams, c: category })
+  }
   const fetchMovies = async () => {
     const moviesList = (await movies$) as Movie[]
     setMovies(moviesList)
   }
+
   useEffect(() => {
     fetchMovies()
   }, [])
+
   return (
     <main>
-      <header>
-        <select>
+      <header className="w-full py-4 bg-gray-700 pl-40">
+        <select
+          onChange={(e) => filterMovies(e.target.value)}
+          className="border-none outline-none px-2 py-2"
+        >
+          <option defaultChecked value="default">
+            Selctionner une catégorie
+          </option>
           {moviesCategories.map((categorie, i) => (
             <option key={i}>{categorie}</option>
           ))}
         </select>
       </header>
       <section className="flex flex-wrap gap-4 mx-auto w-10/12">
-        {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie} />
-        ))}
+        {movies
+          .filter((movie) =>
+            categoryFilter ? movie.category === categoryFilter : movie
+          )
+          .map((movie) => (
+            <MovieCard key={movie.id} movie={movie} />
+          ))}
       </section>
     </main>
   )
--- src/components/movie-card.tsx
+++ src/components/movie-card.tsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Movie } from "../types"
-import { HandThumbDownIcon, HandThumbUpIcon } from "@heroicons/react/24/outline"
+import {
+  HandThumbDownIcon,
+  HandThumbUpIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline"
 import { useCustomContext } from "@/custom-context"
+import freekeys from "freekeys"
+
+const API_KEY = "83e2a63"
 
 const MovieCard = ({ movie }: { movie: Movie }) => {
   const { movies, setMovies } = useCustomContext()
+  const [imageUrl, setImageUrl] = useState("")
+
   const ratioLikes = (movie.likes / (movie.likes + movie.dislikes)) * 100
+
+  const deleteMovie = (id: string) => {
+    setMovies(movies.filter((movie) => movie.id !== id))
+  }
 
   const handleLikes = (like: boolean) => {
     setMovies(
@@ -20,8 +33,24 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
       })
     )
   }
+  const fetchPoster = async () => {
+    try {
+      const { imdb_key, tmdb_key } = await freekeys()
+
+      const { body } = await fetch(
+        `http://img.omdbapi.com/?apikey=${tmdb_key}&s=${movie.title}`
+      )
+      // console.log(body)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  useEffect(() => {
+    // fetchPoster()
+  }, [])
+
   return (
-    <div className="p-2 rounded-lg shadow-md hover:shadow-xl transition-all space-y-1">
+    <div className="p-2 relative rounded-lg shadow-md hover:shadow-xl transition-all space-y-1">
       <img
         className="object-cover w-40 aspect-[9/16]"
         src="https://m.media-amazon.com/images/M/MV5BMTk0MDQ3MzAzOV5BMl5BanBnXkFtZTgwNzU1NzE3MjE@._V1_SX300.jpg"
@@ -32,7 +61,7 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
       <div className="flex items-center gap-2 w-full">
         <button
           onClick={() => handleLikes(true)}
-          className="p-1 rounded hover:bg-gray-100"
+          className="p-1 rounded text-green-600 hover:bg-gray-100"
         >
           <HandThumbUpIcon className="size-4" />
         </button>
@@ -44,9 +73,15 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
         </div>
         <button
           onClick={() => handleLikes(false)}
-          className="p-1 rounded hover:bg-gray-100"
+          className="p-1 rounded hover:bg-gray-100 text-red-600"
         >
           <HandThumbDownIcon className="size-4" />
+        </button>
+        <button
+          onClick={() => deleteMovie(movie.id)}
+          className="p-1 hover:bg-red-100 rounded hover:text-red-500 text-gray-500 transition-all"
+        >
+          <TrashIcon className="size-4 stroke-2" />
         </button>
       </div>
     </div>
